e5924af57400fc04b0916ddd2f345257b51cfd62
MichaelCBrister <70389996+MichaelCBrister@users.noreply.github.com>
2020-09-09 13:37:29 -0500

    Completed Task 2

--- index.js
+++ index.js
@@ -55,12 +55,10 @@ Your function should add the flavor to the front of the array and console.log th
 
 For example addFlavor("Rainbow Sherbert", originalFlavors) should return ["Rainbow Sherbert", "Banana Nut Fudge",..."Vanilla Burnt Almond"] */ 
 
-function addFlavor(/*code here*/){
-
-    /*code here*/
-
+function addFlavor(newFlavor, originalFlavors){
+    originalFlavors.unshift(newFlavor);
+    console.log(originalFlavors);
 }
-
 
 /* Task 3: Houston, we have a problem! There are now 32 flavors in the array! Your task is to remove an item from the end of the array. 
 
